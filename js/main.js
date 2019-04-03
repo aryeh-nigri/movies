bcd92ad53f89ac5dca9006eca279b9435cfadec6
Aryeh Nigri <aryehnigri@gmail.com>
2019-04-03 12:08:25 +0300

    changed http requests to https

--- js/main.js
+++ js/main.js
@@ -11,9 +11,7 @@ function getMovies(searchText) {
     .then(res => res.json())
     .then(res => {
       // console.log(res.Search);
-      // let movies = res.Search;
-      let movies = Avengers;
-      console.log(movies);
+      let movies = res.Search;
       // console.log(movies[0]);
       let output = "";
       movies.forEach(movie => {
@@ -49,9 +47,7 @@ function getMovie() {
     .then(res => res.json())
     .then(res => {
       // console.log(res);
-      // let movie = res;
-      let movie = TheAvengers;
-      console.log(movie);
+      let movie = res;
 
       let output = "";
       output += `
@@ -114,7 +110,7 @@ function getMovie() {
         <h3>Plot</h3>
         ${movie.Plot}
         <hr>
-        <a href="http://imdb.com/title/${
+        <a href="https://imdb.com/title/${
           movie.imdbID
         }" target="_blank" class="btn btn-primary">View IMDB</a>
         <a href="index.html" class="btn btn-default">Go Back To Search</a>
@@ -132,122 +128,4 @@ function getMovie() {
 
 // OMDB API
 const apiKey = "f78a4c1f";
-const url = "http://www.omdbapi.com?apikey=" + apiKey;
-
-// For STATIC GitHub Pages
-const Avengers = [
-  {
-    Title: "The Avengers",
-    Year: "2012",
-    imdbID: "tt0848228 ",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-  },
-  {
-    Title: "Avengers: Age of Ultron",
-    Year: "2015",
-    imdbID: "tt2395427 ",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
-  },
-  {
-    Title: "Avengers: Infinity War",
-    Year: "2018",
-    imdbID: "tt4154756 ",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-  },
-  {
-    Title: "The Avengers",
-    Year: "1998",
-    imdbID: "tt0118661 ",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWE1NTdjOWQtYTQ2Ny00Nzc5LWExYzMtNmRlOThmOTE2N2I4XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
-  },
-  {
-    Title: "The Avengers: Earth's Mightiest Heroes",
-    Year: "2010–2012",
-    imdbID: "tt1626038 ",
-    Type: "series",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYzA4ZjVhYzctZmI0NC00ZmIxLWFmYTgtOGIxMDYxODhmMGQ2XkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg"
-  },
-  {
-    Title: "Ultimate Avengers",
-    Year: "2006",
-    imdbID: "tt0491703 ",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNDFmZTkxMjktMzRiYS00YzMwLWFhZDctOTQ2N2NlOTAyZDJhXkEyXkFqcGdeQXVyNjgzNDU2ODI@._V1_SX300.jpg"
-  },
-  {
-    Title: "Ultimate Avengers II",
-    Year: "2006",
-    imdbID: "tt0803093 ",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  },
-  {
-    Title: "The Avengers",
-    Year: "1961–1969",
-    imdbID: "tt0054518 ",
-    Type: "series",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZWI4ZWM4ZWQtODk1ZC00MzMxLThlZmMtOGFmMTYxZTAwYjc5XkEyXkFqcGdeQXVyMTk0MjQ3Nzk@._V1_SX300.jpg"
-  },
-  {
-    Title: "Avengers Assemble",
-    Year: "2013–",
-    imdbID: "tt2455546 ",
-    Type: "series",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTY0NTUyMDQwOV5BMl5BanBnXkFtZTgwNjAwMTA0MDE@._V1_SX300.jpg"
-  },
-  {
-    Title: "Avengers Assemble",
-    Year: "2013–",
-    imdbID: "tt2455546 ",
-    Type: "series",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTY0NTUyMDQwOV5BMl5BanBnXkFtZTgwNjAwMTA0MDE@._V1_SX300.jpg"
-  }
-];
-
-const TheAvengers = {
-  Title: "The Avengers",
-  Year: "2012",
-  Rated: "PG-13",
-  Released: "04 May 2012",
-  Runtime: "143 min",
-  Genre: "Action, Adventure, Sci-Fi",
-  Director: "Joss Whedon",
-  Writer: "Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)",
-  Actors: "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
-  Plot:
-    "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-  Language: "English, Russian, Hindi",
-  Country: "USA",
-  Awards: "Nominated for 1 Oscar. Another 38 wins & 79 nominations.",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-  Ratings: [
-    { Source: "Internet Movie Database", Value: "8.1/10" },
-    { Source: "Rotten Tomatoes", Value: "92%" },
-    { Source: "Metacritic", Value: "69/100" }
-  ],
-  Metascore: "69",
-  imdbRating: "8.1",
-  imdbVotes: "1,157,818",
-  imdbID: "tt0848228 ",
-  Type: "movie",
-  DVD: "25 Sep 2012",
-  BoxOffice: "$623,279,547",
-  Production: "Walt Disney Pictures",
-  Website: "http://marvel.com/avengers_movie",
-  Response: "True"
-};
+const url = "https://www.omdbapi.com?apikey=" + apiKey;
